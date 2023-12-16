5d543998574cbad0a8169155449a37f0c24dd725
Linsen.Mao <andrew.ls.mao@gmail.com>
2023-12-16 15:38:34 +0800

    update image

--- frontend/react-app/src/components/edit/CreateStory.js
+++ frontend/react-app/src/components/edit/CreateStory.js
@@ -1,144 +1,131 @@
-import React, {useState, useRef, useEffect} from "react";
-import './CreateStory.css'
+import React, { useState, useRef, useEffect } from "react";
+import "./CreateStory.css";
 import { useNavigate } from "react-router-dom";
 
 const CreateStory = () => {
+  // upload image
+  const inputRef = useRef(null);
 
-    // upload image
-    const inputRef = useRef(null);
+  const [imageFile, setImageFile] = useState(null); // 存储文件而不是文件的DataURL
+  const [imagePreview, setImagePreview] = useState(
+    "https://t4.ftcdn.net/jpg/05/65/22/41/360_F_565224180_QNRiRQkf9Fw0dKRoZGwUknmmfk51SuSS.jpg"
+  );
 
-    const [image, setImage] = useState("https://t4.ftcdn.net/jpg/05/65/22/41/360_F_565224180_QNRiRQkf9Fw0dKRoZGwUknmmfk51SuSS.jpg");
+  const handleImageClick = () => {
+    inputRef.current.click();
+  };
 
-    const handleImageClick = () => {
-        inputRef.current.click();
+  const handleImageChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setImageFile(selectedFile); // 存储文件
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result); // 设置预览图
+    };
+    if (selectedFile) {
+      reader.readAsDataURL(selectedFile);
     }
+  };
 
-    const handleImageChange = (event) => {
-        const selectedFile = event.target.files[0];
-        const reader = new FileReader();
-    
-        reader.onloadend = () => {
-          setImage(reader.result); // Set the image in state to display on the frontend
-        };
-    
-        if (selectedFile) {
-          reader.readAsDataURL(selectedFile); // Convert the file to a data URL
-        }
+  // get category id
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [categories, setCategories] = useState([]);
+
+  const handleCategoryChange = (categoryId) => {
+    setSelectedCategory(categoryId);
+  };
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/categories", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json();
+        // Validate the response structure
+        const categories = data && data.categories ? data.categories : [];
+        setCategories(categories);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+        return [];
+      }
     };
 
+    fetchCategories();
+  }, []);
 
-    // get category id
-    const [selectedCategory, setSelectedCategory] = useState('All');
-    const [categories, setCategories] = useState([]);
+  // create new story
+  const navigate = useNavigate();
 
-    const handleCategoryChange = (categoryId) => {
-        setSelectedCategory(categoryId);
-    };
+  const createNewStory = async () => {
+    const title = document.getElementById("inputTitle").value;
+    const description = document.getElementById("inputDescription").value;
 
-    useEffect(() => {   
-        const fetchCategories = async () => {
-            try {
-              const response = await fetch('http://localhost:4000/categories', {
-                method: 'GET',
-                credentials: 'include'
-              });
-              const data = await response.json();
-              // Validate the response structure
-              const categories = data && data.categories ? data.categories : [];
-              setCategories(categories);
-            } catch (error) {
-              console.error('Error fetching categories:', error);
-              return [];
-            }
-        };
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("categoryId", selectedCategory);
+    formData.append("coverImage", imageFile); // 使用文件而不是DataURL
+    formData.append("description", description);
 
-        fetchCategories();
-    })
-
-
-
-
-    // create new story
-    const navigate = useNavigate();
-
-    const createNewStory = async () => {
-
-        // step 1: send data to backend
-        const title = document.getElementById('inputTitle').value;
-        const description = document.getElementById('inputDescription').value;
-
-        const data = {
-            "title": title,
-            "categoryId": selectedCategory,
-            "coverImage": image,
-            "description": description
-        }
-        console.log(data);
-        //TODO send data to backend
-
-        let url = 'http://localhost:4000/stories';
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json', // 告訴後端是json形式
-                    },
-                credentials: 'include',
-                body: JSON.stringify(data),
-            });
-            if(response.ok) {
-                console.log("story was created successfully!!");
-                // setp 2: go to upload images slide TODO
-                navigate("/uploadImages");
-                
-            } else {
-                // Handle error response
-                alert("Something's wrong! Please try again.", response.status);
-                console.error('Request failed:', response.status);
-            }
-        } catch (error) {
-            // Handle fetch error
-            alert("Fetch error", error);
-            console.error('Error:', error);
-        }
+    let url = "http://localhost:4000/stories";
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+        body: formData, // 发送FormData对象
+      });
+      if (response.ok) {
+        console.log("story was created successfully!!");
+        // setp 2: go to upload images slide TODO
+        navigate("/uploadImages");
+      } else {
+        // Handle error response
+        alert("Something's wrong! Please try again.", response.status);
+        console.error("Request failed:", response.status);
+      }
+      // 响应处理...
+    } catch (error) {
+      // 错误处理...
     }
+  };
 
-    return(
+  return (
+    <div className="createStoryDiv">
+      <div className="imageDiv">
+        <img
+          className="coverImg"
+          src={imagePreview}
+          onClick={handleImageClick}
+        />
+        <input
+          type="file"
+          className="inputFileRight"
+          ref={inputRef}
+          onChange={handleImageChange}
+          style={{ display: "none" }}
+        />
+      </div>
 
-        <div className="createStoryDiv">
-            <div className="imageDiv">
-                <img className="coverImg" src={image} onClick={handleImageClick}/>
-                <input
-                    type="file"
-                    className='inputFileRight'
-                    ref={inputRef}
-                    onChange={handleImageChange}
-                    style={{ display: "none"}}
-                />
-            </div>
-
-            <div className="inputDiv">
-                <input id="inputTitle" type="text" placeholder="title"/>
-                <input id="inputDescription" type="text" placeholder="description"/>
-                <select
-                id="categorySelect"
-                value={selectedCategory}
-                onChange={(e) => handleCategoryChange(e.target.value)}
-                >
-                    <option value="All">ALL</option>
-                    {categories.map((category, index) => (
-                        <option key={index} value={category.id}>
-                        {category.name}
-                        </option>
-                    ))}
-                </select>
-                <button onClick={createNewStory}>create</button>
-            </div>
-            
-            
-        </div>
-    );
-
-
-}
+      <div className="inputDiv">
+        <input id="inputTitle" type="text" placeholder="title" />
+        <input id="inputDescription" type="text" placeholder="description" />
+        <select
+          id="categorySelect"
+          value={selectedCategory}
+          onChange={(e) => handleCategoryChange(e.target.value)}
+        >
+          <option value="All">ALL</option>
+          {categories.map((category, index) => (
+            <option key={index} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+        <button onClick={createNewStory}>create</button>
+      </div>
+    </div>
+  );
+};
 export default CreateStory;
